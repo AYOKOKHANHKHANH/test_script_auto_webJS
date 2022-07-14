be0470875ccf134e82a5b530792b7893ff64a5ca
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-07-14 16:39:33 +0700

    Add files via upload

---
+++ pages/login/logout-locator.js
@@ -0,0 +1,21 @@
+class LogoutLocator{
+    getAvatarButtonXpath(){
+            return "/html/body/div/div/div[1]/div/div/img";
+    };
+
+    getAvatarNotImgXpath(){
+        return "/html/body/div/div/div[1]/div/div/span";
+    };
+
+    getLogoutButtonXpath(){
+        return "/html/body/div/div/div[2]/div[2]/div/div/div/div[5]/div[6]";
+    };
+
+    getOkButtonId(){
+        return "modal-button-ok";
+    };
+
+   
+
+};
+module.exports= new LogoutLocator();
