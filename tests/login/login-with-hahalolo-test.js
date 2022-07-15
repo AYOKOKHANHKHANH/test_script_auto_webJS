f6c0ce7ae370bc6c2fca5f93ae2157218da3b7ed
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-07-15 16:38:12 +0700

    Add files via upload

--- tests/login/login-with-hahalolo-test.js
+++ tests/login/login-with-hahalolo-test.js
@@ -1,7 +1,8 @@
 const LoginPage = require('../../pages/login/login-with-hahalolo-page');
 const LogoutPage = require('../../pages/login/logout-page');
 const assert = require ('assert')
-const DriverManager = require('../../driver-manager/setup-driver')
+const DriverManager = require('../../driver-manager/setup-driver');
+const { until } = require('selenium-webdriver');
 
 
 describe('Login',function(){
@@ -34,28 +35,22 @@ describe('Login',function(){
 
 
     it('Login when account never logged in(success)', async function(){
-       await loginPage.login(username,pass,pin);
-    
+       await loginPage.login(username,pass);
+       await loginPage.loginSuccess(pin);
+
        assert.strictEqual(await loginPage.getUrlCurrent(),url)
        await logoutPage.logout();
     })
 
     
     it('Re-login the account that was previously logged in', async function(){
-        await loginPage.login(username, pass, pin)
+        await loginPage.login(username, pass)
+        await loginPage.loginSuccess(pin)
         await logoutPage.logout()
-        await loginPage.clickLoginWithHahaButton();
-        try{
-            await loginPage.clickContinueButton();
-            try{
-                await loginPage.enterPinCode(pin);
-                await loginPage.clickAcceptButton();
-            }
-            catch(e){
-            }
-        }
-        catch(e){
-        }  
+        await loginPage.clickLoginWithHahaButton()
+        await loginPage.loginSuccess(pin)
+
+
         assert.strictEqual(await loginPage.getUrlCurrent(),url)
         await logoutPage.logout();
     })
@@ -64,7 +59,7 @@ describe('Login',function(){
     it('Login not username & password',async function(){
         let username = "";
         let pass = "";
-        await loginPage.login(username,pass,pin);
+        await loginPage.login(username,pass);
 
         assert.strictEqual(await loginPage.getTextNotiWhenNotEnterUsername(), notiWhenNotEnterUsername)
         assert.strictEqual(await loginPage.getTextNotiWhenNotEnterPassword(), notiWhenNotEnterPassword)
@@ -73,7 +68,7 @@ describe('Login',function(){
 
     it('Login not user name', async function(){
         let username = "";
-        await loginPage.login(username, pass, pin);
+        await loginPage.login(username, pass);
 
         assert.strictEqual(await loginPage.getTextNotiWhenNotEnterUsername(), notiWhenNotEnterUsername)
     })
@@ -81,7 +76,7 @@ describe('Login',function(){
 
     it('Login not password', async function(){
         let pass = "";
-        await loginPage.login(username,pass,pin);
+        await loginPage.login(username,pass);
 
         assert.strictEqual(await loginPage.getTextNotiWhenNotEnterPassword(), notiWhenNotEnterPassword)
     })
@@ -89,32 +84,67 @@ describe('Login',function(){
 
     it('Login username True, password False', async function(){
         let pass = "hhhh";
-        await loginPage.login(username,pass,pin);
-        
-        assert.strictEqual(await loginPage.getTextNotiWhenLoginFail(), notiWhenUsernameOrPasswordFalse)
+        await loginPage.login(username,pass);
+        let noti
+        if (await driver.getCurrentUrl() != url){
+            noti = await loginPage.getTextNotiWhenLoginFail()
+        }
+      
+        assert.strictEqual(noti, notiWhenUsernameOrPasswordFalse)
     })
 
 
     it('Login username False, password True',async function(){
        let username = "jjjjj";
-       await loginPage.login(username, pass, pin);
+       await loginPage.login(username, pass);
+       let noti
+       if (await driver.getCurrentUrl != url){
+            noti = await loginPage.getTextNotiWhenLoginFail()
+       }
 
-       assert.strictEqual(await loginPage.getTextNotiWhenLoginFail(), notiWhenUsernameOrPasswordFalse)
+       assert.strictEqual(noti , notiWhenUsernameOrPasswordFalse)
     })
 
 
     it('Login not Pin', async function(){
         let pin = "";
-        await loginPage.login(username, pass, pin);
-
+        await loginPage.login(username, pass);
+        await loginPage.loginSuccess(pin)
         assert.notDeepStrictEqual(await driver.getCurrentUrl(), url)
     })
 
 
     it('Login Pin false',async function(){
         let pin = "111111";
-        await loginPage.login(username, pass, pin);
+        await loginPage.login(username, pass);
+        await loginPage.loginSuccess(pin)
 
         assert.notDeepStrictEqual(await driver.getCurrentUrl(), url)
     } )
+
+    it('Test Continue Button', async function(){
+        await loginPage.login(username, pass)
+        await loginPage.clickContinueButton()
+        await driver.sleep(500)
+
+        let title = await loginPage.getTitleEnterPin()
+        assert.strictEqual(title, 'Nhập mã Pin')
+    })
+
+    it('Test Back Button', async function(){
+        await loginPage.login(username, pass)
+        await loginPage.clickContinueButton()
+        await loginPage.clickBackButton()
+
+        await driver.wait(until.urlIs(url + 'welcome'))
+        assert.strictEqual(await driver.getCurrentUrl(), url + 'welcome')
+    })
+
+    it('Test Not Me Button', async function(){
+        await loginPage.login(username, pass)
+        await loginPage.clickNotMeButton()
+
+        let title = await driver.wait(until.titleIs('Đăng nhập | Hahalolo'))
+        assert.strictEqual(title, true)
+    })
 })
