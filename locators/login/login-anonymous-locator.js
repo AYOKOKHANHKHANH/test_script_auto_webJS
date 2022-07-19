88205591db7dc1448216af727aba5f7a24332233
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-07-19 16:39:24 +0700

    feat(login anonymous)!: login in incognito mode

---
+++ locators/login/login-anonymous-locator.js
@@ -0,0 +1,35 @@
+class LoginAnonymousLocator{
+    getLoginAnonyButtonId(){
+        return "platforms-halome"
+    }
+
+    getAreaCodeXpath(){
+        return "/html/body/div/div/div[2]/div/div/form/div[1]/div/button"
+    }
+
+    getPhoneNumberId(){
+        return "phone-login-form-input-phone"
+    }
+
+    getStartButtonId(){
+        return "phone-login-form-button-ok"
+    }
+
+    getOtpXpath(){
+        return "/html/body/div/div/div[2]/div/div/div[3]/div[1]/div[1]/input"
+    }
+
+    getOtpSecondXpath(){
+        return "/html/body/div/div/div[2]/div/div/div[3]/div[2]/div[1]/input"
+    }
+
+    getResendOtpCodeViaSmsId(){
+        return "phone-verify-form-button-request-sms"
+    }
+
+    getUsernameId(){
+        return "phone-login-form-input-phone"
+}
+}
+
+module.exports = new LoginAnonymousLocator()
