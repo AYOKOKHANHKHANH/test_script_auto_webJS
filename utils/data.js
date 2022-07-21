f47a2aff62a037102f9f5cf92689ed110eb568f6
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-07-21 09:54:46 +0700

    perf(data): Separate data into 1 separate file

---
+++ utils/data.js
@@ -0,0 +1,10 @@
+var DataLogin = {
+    url         : 'https://sb.halome.dev/',
+    username    : '0338061946',
+    password    : 'iloveJennie96',
+    pin         : '123456',
+    phoneNumber : '4560987',
+    otp         : '000000',
+    name        : ''
+}
+module.exports = {DataLogin}
